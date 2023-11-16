3f3a1a4788ee2c3827b610bf1f660684f28bf365
103Sachin <103sachinss@gmail.com>
2023-11-16 19:34:05 +0530

    added external links

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,18 +1,23 @@
 <template>
     <footer>
       <p>&copy; 2023 Your Portfolio. All rights reserved.</p>
-      <!-- Add links to your social media, privacy policy, terms, etc. -->
+      <a href="https://www.linkedin.com/in/sachinsingh103/" target="_blank" rel="noopener noreferrer">
+        <img src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/svgs/brands/linkedin.svg" alt="LinkedIn">
+      </a>
+      <a href="https://github.com/103Sachin" target="_blank" rel="noopener noreferrer">
+        <img src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/svgs/brands/github.svg" alt="GitHub">
+      </a>
     </footer>
-  </template>
+</template>
   
-  <script>
+<script>
   export default {
     // Component logic goes here
     name: 'Footer-Section'
   }
-  </script>
+</script>
   
-  <style scoped>
+<style scoped>
   
   footer {
   background-color: #333;
@@ -24,5 +29,21 @@
 p {
   margin: 0;
 }
-  </style>
+.external-links {
+  margin-top: 1rem;
+  display: flex;
+  justify-content: center;
+}
+
+a {
+  color: white;
+  margin: 0 1rem;
+  text-decoration: none;
+}
+
+a:hover {
+  color: #61dafb;
+}
+</style>
+
   
